99d0bc9fd85166e1b783a85bfd5df0c0e572b116
macinfoboxrecife <macinfoboxrecife@172.28.3.30>
2017-11-27 18:13:00 -0300

    rel

--- src/br/unitins/sac/report/AlunoReport.java
+++ src/br/unitins/sac/report/AlunoReport.java
@@ -1,26 +1,53 @@
 package br.unitins.sac.report;
 
-import org.primefaces.model.StreamedContent;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 
-import br.unitins.sac.util.Report;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class AlunoReport {
-	private String dataSource;
-	private String folder;
-	private String file;
-	private StreamedContent pdf;
-	public AlunoReport(String dataSource, String folder, String file) {
-		this.dataSource = dataSource;
-		this.folder = folder;
-		this.file = file;
-		this.pdf = pdf;
+import br.unitins.sac.util.GerarRelatorio;
+
+@WebServlet("/relatorioAluno")
+public class AlunoReport extends HttpServlet {
+	private static final long serialVersionUID = -5958435742132688971L;
+
+	public class RelatorioAlunoServlet extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+	 
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			doPost(request, response);
+		}
+	 
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+				IOException {
+			try {
+	 
+				// Pega o caminho completo de onde a aplicação está rodando
+				ServletContext servletContext = getServletContext();
+				String diretorio = servletContext.getRealPath(File.separator) + "relatorios/";
+	 
+				// Instaciar a classe que possui os métodos de geração de relatório
+				GerarRelatorio geraRelatorio = new GerarRelatorio();
+	 
+				// Chama o método que gera um array de bytes com o
+				// conteúdo do arquivo PDF
+				byte[] pdf = geraRelatorio.gerarPDF();
+	 
+				OutputStream outStream = response.getOutputStream();
+				response.setHeader("Content-Disposition", "inline, filename=RelatorioAluno.pdf");
+				response.setContentType("application/pdf");
+				response.setContentLength(pdf.length);
+				outStream.write(pdf, 0, pdf.length);
+	 
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
-	public void abrirRelatorio() {
-		AlunoReport report = new AlunoReport("jdbc/web2", "reports", "CidadeReport");
-//		relatorio = new Report("jdbc/web2", "reports", "CidadeReport");
-//		return relatorio;
-	}
-
-
 }
